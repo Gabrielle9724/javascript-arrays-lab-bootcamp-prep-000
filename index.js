074fac391aa1e9735de749dc1eb1b2c073e0eace
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-10 19:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 var kittenstring = "Ralph"
 
 function destructivelyAppendKitten (kittens){
-   return [...kittens, kittenstring]
+   kittens.push("Ralph")
 }
 // Add your functions and code here
